6b6ddf2e96891ec5fb1accb75f8c0e923d52d57c
brand <brand@DESKTOP-A744DQS>
2023-03-12 15:40:11 -0600

    Agregar archivos de proyecto.

---
+++ Aplicacion/Feautres/Libros/Comandos/EliminarLibro/EliminarLibroValidacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Aplicacion.Feautres.Libros.Comandos.EliminarLibro
+{
+    public class EliminarLibroValidacion : AbstractValidator<EliminarLibro>
+    {
+        public EliminarLibroValidacion() 
+        {
+            RuleFor(p => p.Id);
+        }
+    }
+}
